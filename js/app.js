9443268917fdb8178fbe82ff309c54ed23dace05
raaarwwwwandy <hirandysan@gmail.com>
2016-12-14 19:32:42 -1000

    working on app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let theShoppingList = new ShoppingList();
 const contentContainer = document.getElementById("content");
 
-document.getElementById("")
+
 function add_to_shopping_list(){
 let new_shopping_list_item = new ShoppinglistItem(title.value, description.value); 
 let output = addItem(new_shopping_list_item);
